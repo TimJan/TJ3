5fc9afdc62c02a36b5b32aa9da4fca06ba72376b
Tim Jan <tim.jch@gmail.com>
2017-08-25 20:47:38 +0800

    Initial commit

---
+++ app/src/main/java/com/chjan/tj3/Activity000.java
@@ -0,0 +1,13 @@
+package com.chjan.tj3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity000 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout000);
+    }
+}
