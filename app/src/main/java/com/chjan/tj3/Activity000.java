c1db74a48360ff8d56d076f093fa62581d6e9d9e
Tim Jan <tim.jch@gmail.com>
2017-08-26 08:15:03 +0800

    update java code

--- app/src/main/java/com/chjan/tj3/Activity000.java
+++ app/src/main/java/com/chjan/tj3/Activity000.java
@@ -2,12 +2,18 @@ package com.chjan.tj3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Activity000 extends AppCompatActivity {
+
+    Button butInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout000);
+
+        butInfo = (Button)findViewById(R.id.butInfo);
+
     }
 }
